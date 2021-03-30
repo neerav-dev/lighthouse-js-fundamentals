1f30d899958b3c5cfa8087422df53922bf308a04
Neerav Patel <nickpatel.developer@gmail.com>
2021-03-30 12:14:16 -0400

    ice cream array solution code

---
+++ ice-cream.js
@@ -0,0 +1,16 @@
+// Do not modify these first two lines
+const iceCreamFlavours = ["chocolate", "vanilla", "cookies and cream", "rocky road", "strawberry"];
+console.log(iceCreamFlavours);
+
+//Add a flavour
+iceCreamFlavours.push('root beer');
+console.log(iceCreamFlavours);
+
+//Get first flavour
+console.log(iceCreamFlavours[0]);
+
+//Get last flavour
+console.log(iceCreamFlavours[iceCreamFlavours.length - 1]);
+
+//Total no. of flavour
+console.log(iceCreamFlavours.length);
